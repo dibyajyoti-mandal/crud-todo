79efdf569b91b32ac8c4cc2a6a7d8d854fe8a9b0
cookie_jar <cptpricecod098@gmail.com>
2023-12-29 19:39:59 +0530

    friday 29/12/23

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="title">WELCOME!</h1>
+        <h1 className="title2">Your tasks:</h1>
+
+        <div className="todos">
+            <div className="todo">
+                <div className="check"></div>
+                <div className="text">task1</div>
+                <div className="deleteTodo">del</div>
+            </div>
+        </div>
+
     </div>
   );
 }
