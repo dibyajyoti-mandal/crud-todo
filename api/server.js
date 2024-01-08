031087131e1d82350db14ea00446fe236acd67cb
cookie_jar <cptpricecod098@gmail.com>
2024-01-08 19:38:12 +0530

    async await added

--- api/server.js
+++ api/server.js
@@ -7,8 +7,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://dmandal:dmandal2104@cluster0.flbmuji.mongodb.net/?retryWrites=true&w=majority', {
-}).then(() => console.log("Connected to MongoDB")).catch(console.error);
+(async()=> {
+	try {
+		await mongoose.connect('mongodb+srv://dmandal:dmandal2104@cluster0.flbmuji.mongodb.net/?retryWrites=true&w=majority', {
+		}).then(() => console.log("Connected to MongoDB"));
+	}
+	catch (err) {
+		console.log(err);
+	}
+})()
+
+// mongoose.connect('mongodb+srv://dmandal:dmandal2104@cluster0.flbmuji.mongodb.net/?retryWrites=true&w=majority', {
+// }).then(() => console.log("Connected to MongoDB")).catch(console.error);
 
 const Todo = require('./models/todo')
 
@@ -27,7 +37,7 @@ app.post('/todo/new', (req, res) => {
 
 app.delete('/todo/delete/:id', async (req, res) => {
 	const result = await Todo.findByIdAndDelete(req.params.id);
-	res.json({result});
+	res.json({ result });
 });
 
 app.get('/todo/complete/:id', async (req, res) => {
@@ -44,4 +54,6 @@ app.put('/todo/update/:id', async (req, res) => {
 	res.json(todo);
 });
 
-app.listen(3001);
+app.listen(3001, ()=>{
+	console.log("connected to server")
+})
